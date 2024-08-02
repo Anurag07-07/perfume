fcff24981da4f33ed1e40a6f8c8870bbdcadb811
Anurag07-07 <adityanv*@gmail.com>
2024-08-03 02:29:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// let modal = document.querySelector('.modal');
+// let overlay = document.querySelector('.overlay');
+
+// const open = ()=>{
+//     console.log("modal is open");
+//     modal.classList.add("active");
+//     modal.classList.add("activeoverlay");
+// }
+
+// const close = ()=>{
+//     modal.classList.remove("active");
+//     modal.classList.remove("activeoverlay");
+// }
+
+let text = document.getElementById('auto_radius');
+const rotate = new CircleType(text).radius(60)
+
+let slides = document.querySelectorAll('#slide');
+
+
